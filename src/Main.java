5f40e85b245d7e4624c2dd7c468e26ff7abf3c9b
mmoafy <20235037@stud.fci-cu.edu.eg>
2025-10-21 21:43:17 +0300

    ls cp cp -r all done

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.io.File;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.Files;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Arrays;
 
 class Parser
 {
@@ -217,12 +222,146 @@ class Terminal
         }
 
     }
+    public void ls(String[] args)
+    {
+
+        if(args.length > 0)
+        {
+            System.out.println("this command takes no arguments"); // Check for arguments
+            return;
+        }
+
+        File dir = new File(currentDirectory); // find current directory
+        String[] contents = dir.list(); //  array of file names.
+
+        if(contents != null)
+        {
+            List<String> list = Arrays.asList(contents); // sort alphabetically
+            Collections.sort(list);
+            for(String item : list)
+            {
+                System.out.println(item); // print items
+            }
+        }
+        else
+        {
+            System.out.println("Error: Can't list directory contents");
+        }
+
+    }
+
+
+    public void cp(String[] args)
+    {
+
+        if(args.length != 2)
+        {
+            System.out.println("cp: must be 2 arguments"); // validate the arguments must be cp <source> <destination>
+            return;
+        }
+
+        if(args[0].equals("-r"))
+        {
+            System.out.println("cp: missing arguments");
+            return;
+        }
+
+        // make the file pathes
+        File source = new File(args[0]);
+        File destination = new File(args[1]);
+
+        if(!source.isAbsolute()) source = new File(currentDirectory, args[0]);
+        if(!destination.isAbsolute()) destination = new File(currentDirectory, args[1]);
+
+        if(!source.exists() || !source.isFile())  // make sure source file exists
+        {
+            System.out.println("cp: source file does not exist");
+            return;
+        }
+
+        try
+        {
+            Files.copy(source.toPath(), destination.toPath(), StandardCopyOption.REPLACE_EXISTING); // copy the file
+        }
+        catch(IOException e)
+        {
+            System.out.println("cp: error");
+        }
+
+    }
+
+    public void cp_r(String[] args)
+    {
+
+        if(args.length != 3 || !args[0].equals("-r")) // validate argu and format
+        {
+            System.out.println("cp -r: must be cp -r <sourceDir> <destinationDir>");
+            return;
+        }
+        // for paths
+        File sourceDir = new File(args[1]);
+        File destDir = new File(args[2]);
+
+        if(!sourceDir.isAbsolute()) sourceDir = new File(currentDirectory, args[1]);
+        if(!destDir.isAbsolute()) destDir = new File(currentDirectory, args[2]);
+
+        if(!sourceDir.exists() || !sourceDir.isDirectory()) // check if the source is a directory
+        {
+            System.out.println("cp -r: source is not a valid ");
+            return;
+        }
+
+        try
+        {
+            copyDirectoryRecursive(sourceDir, new File(destDir, sourceDir.getName()));
+        }
+        catch(IOException e)
+        {
+            System.out.println("cp -r: error copying directory");
+        }
+
+    }
+    // Helper Method for Recursive Copy
+    private void copyDirectoryRecursive(File source, File dest) throws IOException
+    {
+
+        if(!dest.exists()) dest.mkdir();
+
+        File[] files = source.listFiles();
+
+        if(files != null)
+        {
+            for(File file : files)
+            {
+                File newDest = new File(dest, file.getName());
+
+                if(file.isDirectory())
+                {
+                    copyDirectoryRecursive(file, newDest);
+                }
+                else
+                {
+                    Files.copy(file.toPath(), newDest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                }
+            }
+        }
+
+    }
+
     public void chooseCommandAction()
     {
 
         if(parser.getCommandName().equals("pwd")) System.out.println(pwd());
         else if(parser.getCommandName().equals("cd")) cd(parser.getArgs());
         else if(parser.getCommandName().equals("mkdir")) mkdir(parser.getArgs());
+
+        else if(parser.getCommandName().equals("ls")) ls(parser.getArgs());
+        else if(parser.getCommandName().equals("cp"))
+        {
+            String[] arguments = parser.getArgs();
+            if(arguments.length > 0 && arguments[0].equals("-r")) cp_r(arguments);
+            else cp(arguments);
+        }
 
     }
     public static void main(String[] args)
